4880418975e79f95379d27e71d5279feb7b4327b
Yasuda Kouki <e225703@ie.u-ryukyu.ac.jp>
2022-11-01 10:01:38 +0900

    もう一回やった

--- src/Chatbot.java
+++ src/Chatbot.java
@@ -2,4 +2,8 @@ public class Chatbot {
     public Chatbot() {
         System.out.println("チャットボットの原型を作成します。");
     }
+
+    public Chatbot(String name){
+        System.out.println("チャットボット名" + name + "を作成します。");
+    }
 }
