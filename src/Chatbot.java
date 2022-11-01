70b09bf07a88522c5406333e9c3d524381d6af54
Yasuda Kouki <e225703@ie.u-ryukyu.ac.jp>
2022-11-01 09:32:31 +0900

    1st commit

---
+++ src/Chatbot.java
@@ -0,0 +1,5 @@
+public class Chatbot {
+    public Chatbot() {
+        System.out.println("チャットボットの原型を作成します。");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("main method runs");
+
+        Chatbot bot1 = new Chatbot();
+        
+
+
+
+    }
+}
